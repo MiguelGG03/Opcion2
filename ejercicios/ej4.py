03ebfbfcb3f6cfd03b580fe319dbc9aea7f9aa35
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-09 18:21:36 +0100

    Bibliografia y Detalles

--- ejercicios/ej4.py
+++ ejercicios/ej4.py
@@ -6,8 +6,11 @@ def fibonacci_iterativo(n):
     else:
         a = 0
         b = 1
+        print('Secuencia de Fibonacci hasta', n, ':\n')
         for i in range(2, n+1):
             c = a + b
             a = b
             b = c
         return c
+
+# Sacado de https://www.programiz.com/python-programming/examples/fibonacci-sequence
