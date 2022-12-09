6819b68c84309cd163a693440b102d63ebfd4b2e
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-09 19:00:05 +0100

    lo he separado todo

--- ejercicios/ej2.py
+++ ejercicios/ej2.py
@@ -3,25 +3,23 @@
 
 def expand(func:str):
     for i in range(len(func)):
+        if func[i]=='(':
+            marc1=i
+        elif func[i]==')':
+            marc2=i
+        elif func[i]=='^':
+            marc3=func[i+1:]
+    for i in range(len(func)):
         if func[i] == '^':
             func = func[:i] + '**' + func[i+1:]
-    if n == k:
-        return 1
-    elif k == 0:
-        return 1
-    else:
-        a = 1
-        b = 1
-        for i in range(2, n+1):
-            c = a * (n - i + 1) / i
-            a = b
-            b = c
-        return c
+    print(marc1)
+    print(marc2)
+    print(marc3)
 
 def main2():
-    print('Sabiendo que ')
-    n = int(input('Dame el valor de n\n'
-                    '>>>'))
-    k = int(input('Dame el valor de k\n'
-                    '>>>'))
-    print(binomio_newton_iterativo(n, k))
+    pr1 = input('Que funcion quieres expandir?\n'
+                '>>>')
+    expand(pr1)
+
+if __name__ == '__main__':
+    main2()
